0d425c941a75d9d1a2d4f683ced0f426dfc9fad7
Frolenko Dima <mail@frolenkodima.ru>
2023-09-10 03:29:40 +0300

    fix: storage переработана работа с ошибками теперь правильно упаковываются ошибки новая стратегия именования ошибок

--- storage/errors.go
+++ storage/errors.go
@@ -1,27 +1,18 @@
 package storage
 
-var (
-	// список оберток для ошибок s3
-	KeyNotFound = wrapError("Ключ не найден в бакете")
-)
+import "fmt"
 
-// StorageError оперделяет какая ошибка произошла, и какая ошибка в s3 её вызвала
-type StorageError struct {
-	Text string
-	Err  error
+// KeyNotFound это тип ошибки, который возвращается если не найден нужный ключ в бакете, содержит информацию какой ключ и в каком бакете
+type KeyNotFound struct {
+	BucketName string
+	Key        string
+	Err        error
 }
 
-func (se StorageError) Error() string {
-	return se.Text
+func (e KeyNotFound) Error() string {
+	return fmt.Sprintf("Ключ %v не найден в бакете %v", e.Key, e.BucketName)
 }
 
-// wrapError Оборачивает ошибку оригинальную, ошибкой из этого пакета. Так сохраняется преемственность ошибок от S3.
-// newtext опеределяет новый текст ошибки
-func wrapError(newtext string) func(error) error {
-	return func(parent error) error {
-		return StorageError{
-			Text: newtext,
-			Err:  parent,
-		}
-	}
+func (e KeyNotFound) Unwrap() error {
+	return e.Err
 }
