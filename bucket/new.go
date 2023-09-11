40e03a07328ed0bf163c86f58fa7bb0803143b8e
Dima Frolenko <mail@frolenkodima.ru>
2023-09-11 10:55:41 +0300

    feat: Переработка создания Bucket, новые сигнатуры, опцефункции

--- bucket/new.go
+++ bucket/new.go
@@ -2,8 +2,14 @@ package bucket
 
 import (
 	"errors"
+	"fmt"
+	"os"
 
 	"github.com/thefrol/minimal/internal/amazon"
+)
+
+const (
+	defaultConfigFile = ".minimal"
 )
 
 // New Создает новый объект бакета, и дает доступ в бакет с именем name
@@ -11,32 +17,51 @@ import (
 // остальное настраивается при помощи func_opts
 //
 // MNML_KEY, MNML_SECRET - переменные окружения с креденшансами
-func New() (*Bucket, error) {
-	c, err := amazon.Client(amazon.LoadFromEnv)
-	if err != nil {
-		return nil, err
+// MNML_BUCKET - имя бакета
+func WithOptions(funcOpts ...OptionsFunc) (*Bucket, error) {
+	b := new(Bucket)
+	for _, f := range funcOpts {
+		err := f(b)
+		if err != nil {
+			return nil, err
+		}
 	}
-	b := Bucket{
-		s3client: c,
+
+	if b.Name == "" { //validate name! #todo без подчеркиваний там
+		return nil, fmt.Errorf("пустое имя бакета")
 	}
-	return &b, nil
+	return b, nil
 }
 
-// WithName открывает бакет, с креденшалсами по умолчанию(из переменных
-// окружения), но можно настроить имя
-func WithName(name string) (*Bucket, error) {
-	b, err := New()
-	if err != nil {
-		return nil, err
-	}
-	b.Name = name //#todo переделать с func_opts
+// New Создает новый объект бакета, и дает доступ в бакет с именем name
+// Ключ и секрет берутся из переменных окружения по умолчанию, все
+// остальное настраивается при помощи func_opts
+//
+// MNML_KEY, MNML_SECRET - переменные окружения с креденшансами
+// MNML_BUCKET - имя бакета
+func New(name string) (b *Bucket, err error) {
+	b, err = WithOptions(ConfigFromFile(defaultConfigFile), CredentialsFromEnv, NameFromEnv, WithName(name))
+	return
+}
+
+// Default открывает бакет, с креденшалсами по умолчанию(из переменных
+// окружения), или из файла профиля, имя берется тоже оттуда же
+func Default() (*Bucket, error) {
+	b, err := WithOptions(ConfigFromFile(defaultConfigFile), CredentialsFromEnv, NameFromEnv) // #todo а еще добавить в цепочку блять из файла
 	return b, err
 }
 
 // FromEnvironmentVariables Открывает бакет, полностью обусловленный переменными окружения,
-// в том числе и имя бакета тоже берется из MNML_KEY, MNML_SECRET
+// в том числе и имя бакета тоже берется из MNML_BUCKET,
+// MNML_KEY, MNML_SECRET - тут креденшалсы
 func FromEnvironmentVariables() (*Bucket, error) {
-	return nil, errors.New("функция FromEnvironment Variables пока не воплощена")
+	b, err := WithOptions(CredentialsFromEnv, NameFromEnv) // #todo а еще добавить в цепочку блять из файла
+	return b, err
+}
+
+// FromKeys Открывает бакет, так что креденшалы задаются в открытом виде
+func FromKeys(key, secret, session, name string) (*Bucket, error) {
+	return nil, errors.New("функция FromFile Variables пока не воплощена")
 }
 
 // FromFile Открывает бакет, описанный в адресе,
@@ -50,3 +75,43 @@ func FromFile(f string) (*Bucket, error) {
 func FromDefaultFile() (*Bucket, error) {
 	return nil, errors.New("функция FromFile Variables пока не воплощена")
 }
+
+// Опцефункции
+
+type OptionsFunc func(b *Bucket) error
+
+func NameFromEnv(b *Bucket) error {
+	name, found := os.LookupEnv("MNML_BUCKET")
+	if !found {
+		fmt.Println("Не могу получить имя бакета из переменной окружения MNML_BUCKET")
+	}
+
+	return WithName(name)(b)
+}
+
+func WithName(name string) OptionsFunc {
+	return func(b *Bucket) error {
+		if name == "" {
+			return nil // если имя пусток ничего не присваиваем дополнительно, может быть где-то в цепочке уже кто-то что-то ввел в имя
+		}
+		b.Name = name
+		return nil
+	}
+}
+
+func CredentialsFromEnv(b *Bucket) error {
+	c, err := amazon.Client(amazon.LoadFromEnv)
+	if err != nil {
+		return err
+	}
+	b.s3client = c
+	return nil
+}
+
+func ConfigFromFile(path string) OptionsFunc {
+	return func(b *Bucket) error {
+		return fmt.Errorf("не воплощено") //todo
+	}
+}
+
+// #todo все эти функции выделить бы в отдельный файл с интерфейсом для AWS клиентировнных штук
