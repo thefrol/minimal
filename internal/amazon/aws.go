03a510c940e8df2e634a87f709a7c12912623405
Dima Frolenko <mail@frolenkodima.ru>
2023-09-11 14:43:46 +0300

    feat: internal/amazon новые конструкторы клиента

--- internal/amazon/aws.go
+++ internal/amazon/aws.go
@@ -58,4 +58,21 @@ func LoadFromEnv(opts *config.LoadOptions) error {
 	return nil
 }
 
+func LoadFromFile(opts *config.LoadOptions) error {
+	/* 	e := struct {
+		Key     string `env:"MNML_KEY"`
+		Secret  string `env:"MNML_SECRET"`
+		Session string `env:"MNML_SESSION"`
+	}{} */
+
+	return fmt.Errorf("Client.LoadFromFile() не воплощена")
+}
+
+func StaticKeys(key, secret, session string) config.LoadOptionsFunc {
+	return func(opts *config.LoadOptions) error {
+		opts.Credentials = credentials.NewStaticCredentialsProvider(key, secret, session)
+		return nil
+	}
+}
+
 // код взяд из официальной документации yandex cloud
