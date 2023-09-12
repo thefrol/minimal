51e8e10f3bedc3c5925dded8d8dade28f98e5e08
Dima Frolenko <mail@frolenkodima.ru>
2023-09-12 04:29:19 +0300

    fix: bucket Уменьшить число конструкторов для бакета

--- bucket/new.go
+++ bucket/new.go
@@ -1,3 +1,11 @@
+// Модуль для работы с бакетами. МОжно создавать, менять, удалять, получать файлы
+//
+// Бакет создается только нессколькими простыми формулами, из мерепенных окружения, если нужно что0то посложнее
+// можно воспользоваться контруктами уровня модуля minimal, вроде
+//	Profile("test").Bucket("dev")
+//	DefaultProfile().Default().Bucket()
+//	Тут у нас только из переменных окружения или из файлов
+
 package bucket
 
 import (
@@ -47,7 +55,7 @@ func New(name string) (b *Bucket, err error) {
 // Default открывает бакет, с креденшалсами по умолчанию(из переменных
 // окружения), или из файла профиля, имя берется тоже оттуда же
 func Default() (*Bucket, error) {
-	b, err := WithOptions(ConfigFromFile(defaultConfigFile), CredentialsFromEnv, NameFromEnv) // #todo а еще добавить в цепочку блять из файла
+	b, err := WithOptions(CredentialsFromEnv, NameFromEnv) // тут может быть другой
 	return b, err
 }
 
@@ -55,24 +63,12 @@ func Default() (*Bucket, error) {
 // в том числе и имя бакета тоже берется из MNML_BUCKET,
 // MNML_KEY, MNML_SECRET - тут креденшалсы
 func FromEnvironmentVariables() (*Bucket, error) {
-	b, err := WithOptions(CredentialsFromEnv, NameFromEnv) // #todo а еще добавить в цепочку блять из файла
+	b, err := WithOptions(CredentialsFromEnv, NameFromEnv) // #todo а еще добавить в цепочку
 	return b, err
 }
 
 // FromKeys Открывает бакет, так что креденшалы задаются в открытом виде
 func FromKeys(key, secret, session, name string) (*Bucket, error) {
-	return nil, errors.New("функция FromFile Variables пока не воплощена")
-}
-
-// FromFile Открывает бакет, описанный в адресе,
-// в том числе и имя бакета тоже берется из
-func FromFile(f string) (*Bucket, error) {
-	return nil, errors.New("функция FromFile Variables пока не воплощена")
-}
-
-// FromDefaultFile Открывает бакет, описанный в файле по умолчанию .minimal,
-// в том числе и имя бакета тоже берется из
-func FromDefaultFile() (*Bucket, error) {
 	return nil, errors.New("функция FromFile Variables пока не воплощена")
 }
 
@@ -106,12 +102,6 @@ func CredentialsFromEnv(b *Bucket) error {
 	}
 	b.s3client = c
 	return nil
-}
-
-func ConfigFromFile(path string) OptionsFunc {
-	return func(b *Bucket) error {
-		return fmt.Errorf("не воплощено") //todo
-	}
 }
 
 // #todo все эти функции выделить бы в отдельный файл с интерфейсом для AWS клиентировнных штук
